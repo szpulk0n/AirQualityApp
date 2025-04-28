229f0e0be8219dc746d535c478bb3fa2e6141fee
szpulk0n <konrad.szpulecki@gmail.com>
2025-04-28 23:34:34 +0200

    App_icon.png

--- main.cpp
+++ main.cpp
@@ -1,30 +1,29 @@
-/**
- * @file main.cpp
- * @brief Główny plik aplikacji do monitorowania jakości powietrza
- */
+#include <QApplication>
+#include <QIcon>
+#include <QSplashScreen>
+#include <QTimer>
+#include "main_window.hpp"
 
- #include <QApplication>
- #include <QSplashScreen>
- #include <QTimer>
- #include "main_window.hpp"
- 
- int main(int argc, char *argv[]) {
-     QApplication app(argc, argv);
-     
-     // Ustawienie informacji o aplikacji
-     QApplication::setApplicationName("Monitor Jakości Powietrza");
-     QApplication::setApplicationVersion("1.0");
-     
-     // Ekran powitalny
-     QSplashScreen splash(QPixmap("../welcome.png"));
-     splash.show();
-     
-     // Inicjalizacja głównego okna
-     MainWindow mainWindow;
-     
-     // Pokazanie głównego okna po krótkim opóźnieniu
-     QTimer::singleShot(1500, &splash, &QSplashScreen::close);
-     QTimer::singleShot(1500, &mainWindow, &MainWindow::show);
-     
-     return app.exec();
- }
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    // Ustawienie informacji o aplikacji
+    QApplication::setApplicationName("Monitor Jakości Powietrza");
+    QApplication::setApplicationVersion("1.0");
+    
+    // Ustawienie ikony aplikacji
+    app.setWindowIcon(QIcon("../icons/app_icon.png"));
+    
+    // Ekran powitalny
+    QSplashScreen splash(QPixmap("../welcome.png"));
+    splash.show();
+    
+    // Inicjalizacja głównego okna
+    MainWindow mainWindow;
+    
+    // Pokazanie głównego okna po krótkim opóźnieniu
+    QTimer::singleShot(1500, &splash, &QSplashScreen::close);
+    QTimer::singleShot(1500, &mainWindow, &MainWindow::show);
+    
+    return app.exec();
+}
